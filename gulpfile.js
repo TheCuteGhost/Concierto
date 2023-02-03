34bb0e3dcfbb75c12ee133744e12afd9f77116d1
Cute Ghost <elcuteghost@gmail.com>
2023-02-03 15:46:08 -0600

    Añadiendo variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 const {src, dest, watch} = require("gulp"); 
 const sass = require("gulp-sass")(require("sass"));
 
-function css (done) {
+function css (end) {
 
-    src('./src/scss/app.scss')
+    src('./src/**/*.scss')
         .pipe(sass())
         .pipe(dest("build/css")) //Pipe es para que se haga una fucion dentro de los parentecis. Podemos añadir más pipe para que se vuelva a repetir
 
-    done(); //Callback que le avisa a gulp cuando acaba la función
+    end(); //Callback que le avisa a gulp cuando acaba la función
 }
 
-function dev (done) {
-    watch("src/scss/app.scss", css);
+function dev (end) {
+    watch("src/**/*.scss", css);
 
-    done();
+    end();
 }
 
 exports.css = css;  
