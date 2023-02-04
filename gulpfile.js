59891f97a7a8dd86aa42265d6b1840111da1ba35
Cute Ghost <elcuteghost@gmail.com>
2023-02-03 19:44:20 -0600

    Probando

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const {src, dest, watch} = require("gulp"); 
 const sass = require("gulp-sass")(require("sass"));
+const gulp = require('gulp-plumber');
 
 function css (end) {
 
     src('./src/**/*.scss')
+        .pipe(plumber())
         .pipe(sass())
         .pipe(dest("build/css")) //Pipe es para que se haga una fucion dentro de los parentecis. Podemos añadir más pipe para que se vuelva a repetir
 
