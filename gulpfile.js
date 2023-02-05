357d3461056b8d7c1ab6f1b17713e7bd57884626
Cute Ghost <elcuteghost@gmail.com>
2023-02-04 21:38:14 -0600

    Añadiendo gulp-webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const {src, dest, watch} = require("gulp"); 
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
-const gulp = require('gulp-plumber');
+const gulp = require('gulp-plumber')
+
+// Imagenes
+const webp = require('gulp-webp');
 
 function css (end) {
 
@@ -10,6 +15,10 @@ function css (end) {
         .pipe(dest("build/css")) //Pipe es para que se haga una fucion dentro de los parentecis. Podemos añadir más pipe para que se vuelva a repetir
 
     end(); //Callback que le avisa a gulp cuando acaba la función
+}
+
+function versionWebp(end) {
+    src('src/img/**/*.{}')
 }
 
 function dev (end) {
