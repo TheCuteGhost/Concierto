8d47930e2cf9fb08e1c093f00bf82006ccb39196
Cute Ghost <elcuteghost@gmail.com>
2023-02-05 00:01:28 -0600

    webp configurado

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
-const {src, dest, watch} = require("gulp"); 
+const {src, dest, watch, parallel} = require("gulp"); 
 
 // CSS
 const sass = require("gulp-sass")(require("sass"));
-const gulp = require('gulp-plumber')
+const gulp = require('gulp-plumber');
 
 // Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 
 function css (end) {
 
-    src('./src/**/*.scss')
+    src('../src/**/*.scss')
         .pipe(plumber())
         .pipe(sass())
         .pipe(dest("build/css")) //Pipe es para que se haga una fucion dentro de los parentecis. Podemos añadir más pipe para que se vuelva a repetir
@@ -17,15 +19,35 @@ function css (end) {
     end(); //Callback que le avisa a gulp cuando acaba la función
 }
 
-function versionWebp(end) {
-    src('src/img/**/*.{}')
+function imagenes(end) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    end();
+}
+
+function Webp(end) {
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    end();
 }
 
 function dev (end) {
-    watch("src/**/*.scss", css);
+    watch("src/scss/**/*.scss", css);
 
     end();
 }
 
 exports.css = css;  
-exports.dev = dev; 
+exports.Webp = Webp;
+exports.imagenes = imagenes;
+exports.dev = parallel(imagenes, Webp, dev); 
+
