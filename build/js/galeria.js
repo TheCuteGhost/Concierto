cdaacadb46810df025cd72010a668dfc7a6d839a
Cute Ghost <elcuteghost@gmail.com>
2023-02-05 15:32:03 -0600

    Terminando la galeria

--- build/js/galeria.js
+++ build/js/galeria.js
@@ -16,7 +16,45 @@ function crearGaleria() {
 
         <img loading="lazy" src="build/img/thumb/${i}.jpg" alt="imagen galeria">`;
 
+        imagen.onclick = function() {
+            mImg(i);
+        }
+
         galeria.appendChild(imagen);  
         
     }
+}
+
+function mImg(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+    <source srcset="build/img/grande/${id}.avif" type="image/avif">
+    <source srcset="build/img/grande/${id}.webp" type="image/webp">
+
+    <img loading="lazy" src="build/img/grande/${id}.jpg" alt="imagen galeria">`;
+    // Crear el overlay con la imagen
+    const overlay =document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    overlay.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+
+    // Boton para cerrar el Modal
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+    overlay.appendChild(cerrarModal);
+    cerrarModal.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+    // Añadiendo al HTML
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
+
 }
