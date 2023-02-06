6d601f73ec2a36b40c249350787a552d373c5f69
Cute Ghost <elcuteghost@gmail.com>
2023-02-05 21:10:14 -0600

    Header fijo

--- build/js/galeria.js
+++ build/js/galeria.js
@@ -3,8 +3,27 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function iniciarApp() {
+    navegacionFija();
     crearGaleria();
-    ScrollNav();
+    scrollNav();
+}
+
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+    const body = document.querySelector('body');
+    
+    window.addEventListener('scroll', function() {
+        console.log(sobreFestival.getBoundingClientRect());
+
+        if (sobreFestival.getBoundingClientRect().top < 0) {
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('fijo');
+        }
+    })
 }
 
 function scrollNav() {
